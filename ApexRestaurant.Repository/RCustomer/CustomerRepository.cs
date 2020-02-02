d38a5385446f9de71757b6a8d33102e93fddcca4
anshulm0420 <anshulm0420@gmail.com>
2020-02-02 09:32:18 +0545

    added repository for customer

---
+++ ApexRestaurant.Repository/RCustomer/CustomerRepository.cs
@@ -0,0 +1,13 @@
+using ApexRestaurant.Repository.Domain;
+
+namespace ApexRestaurant.Repository.RCustomer
+{
+    public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
+    {
+        public CustomerRepository(RestaurantContext dbContext)
+        {
+            DbContext = dbContext;
+         }
+    }
+    
+}
